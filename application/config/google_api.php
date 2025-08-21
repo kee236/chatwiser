21118dfac18064dcffd53ba9c51786abf43eae8a
kee236 <31869356+kee236@users.noreply.github.com>
2025-08-21 23:49:41 +0700

    Create google_api.php

---
+++ application/config/google_api.php
@@ -0,0 +1,4 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$config['google_client_id'] = 'YOUR_GOOGLE_CLIENT_ID';
+$config['google_client_secret'] = 'YOUR_GOOGLE_CLIENT_SECRET';
